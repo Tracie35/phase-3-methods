35206c5d55afe00d05ecd94647473e10b34fe4d6
Tracie35 <kariukitracie@gmail.com>
2022-08-17 15:26:33 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer 
+
+def greet name
+    puts "Hello, #{name}!"  
+end
+greet "Tracy"
+
+def greet_with_default(default="Tracy")
+    puts "Hello, #{default}!"
+    puts "Hello, programmer!"
+end
+greet_with_default 
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+puts add(1,2)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    number / 2
+end
+puts halve(1)
